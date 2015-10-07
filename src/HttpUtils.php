f3ddd777704597e7482fd063ddc14f41f3921a96
Riccardo De Martis <riccardo@demartis.it>
2015-10-07 17:25:44 +0200

    bug fix getNormalizedData($out);

--- src/HttpUtils.php
+++ src/HttpUtils.php
@@ -6,17 +6,12 @@ class HttpUtils
 
     public static function decodeOut($out)
     {
-        /*
-         * cure response if client send a 100-continue header:
-         * http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.20
-         */
-        $out=self::removeContinue($out);
 
         // It should be a fancy way to do that :(
         $headersFinished = FALSE;
         $headers         = $content = [];
 
-        $data = self::getArrayData($out);
+        $data = self::getNormalizedData($out);
 
         foreach ($data as $line) {
             if (trim($line) == '') {
@@ -35,26 +30,38 @@ class HttpUtils
         return [$headers, implode("\n", $content)];
     }
 
-    public static function removeContinue($out){
-
-        $data = self::getArrayData($out);
+    public static function removeContinue($data){
 
         if(count($data)){
             if(stripos($data[0],"HTTP/1.1 100 Continue")!==false){
                 array_shift($data); //remove header
                 array_shift($data); //remove \n line
             }
-
         }
         return implode("\n", $data);
     }
 
 
-    private static function getArrayData($out){
+    private static function getNormalizedData($out){
+
         if(is_array($out)){
+
             $data=$out;
+            // if $out is array isn't necessary to purify by 100-continue
+
         }else{
+
+            /*
+             * cure response if client send a 100-continue header:
+             * http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.20
+             */
+
             $data  = explode("\n", $out);
+
+            $out=self::removeContinue($data);
+
+            $data  = explode("\n", $out);
+
         }
         return $data;
     }
